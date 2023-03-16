5dddd9d5ec63564f1ad64163c516a12e7444d344
Alexander Mandera <alexander@mandera.eu>
2023-03-16 15:37:46 +0100

    Add interrupt, try to fix USART

--- cores/arduino/api/HardwareSerial.cpp
+++ cores/arduino/api/HardwareSerial.cpp
@@ -14,16 +14,26 @@ void HardwareSerial::begin(unsigned long baud, uint16_t config) {
     GPIOD->CFGLR &= ~(0xf<<(4*5));
     GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF)<<(4*5);
 
-    USART1->CTLR1 = USART_WordLength_8b | USART_Parity_No | USART_Mode_Tx;
+    GPIOD->CFGLR &= ~(0xf<<(4*6));
+    GPIOD->CFGLR |= (GPIO_CNF_IN_FLOATING)<<(4*6);
+
+    USART1->CTLR1 = USART_WordLength_8b | USART_Parity_No | USART_Mode_Rx | USART_Mode_Tx;
     USART1->CTLR2 = USART_StopBits_1;
     USART1->CTLR3 = USART_HardwareFlowControl_None;
 
     USART1->BRR = UART_BRR;
+    USART1->CTLR1 |= USART_FLAG_RXNE;
+
+    // Enable Interrupt
+    NVIC->IPRIOR[USART1_IRQn] = (0 << 7) | 0x05;
+    NVIC->IENR[USART1_IRQn >> 0x05] = 1 << (USART1_IRQn & 0x1F);
+
+    // Enable UART
     USART1->CTLR1 |= CTLR1_UE_Set;
 }
 
 void HardwareSerial::begin(unsigned long baud) {
-    begin(baud, 0); // TODO: Add config options
+    begin(baud, 0);
 }
 
 void HardwareSerial::end() {
@@ -39,15 +49,19 @@ int HardwareSerial::peek() {
 }
 
 int HardwareSerial::read() {
-    return rxBuffer[rxBufferHead++];
+    rxBufferHead = (rxBufferHead + 1) % RX_BUFFER_SIZE;
+    if(rxBufferHead != rxBufferTail) {
+        USART1->CTLR1 |= USART_FLAG_RXNE;
+    }
+    return rxBuffer[rxBufferHead];
 }
 
 void HardwareSerial::flush() {
 }
 
 size_t HardwareSerial::write(uint8_t c) {
-    USART1->DATAR = (uint16_t)(c & (uint16_t)0x01FF);
-    while((USART1->STATR & USART_FLAG_TXE) == RESET);
+    while( !(USART1->STATR & USART_FLAG_TC));
+    USART1->DATAR = c;
     return 1;
 }
 
@@ -63,7 +77,7 @@ HardwareSerial::operator bool() {
     return true;
 }
 
-/*void USART1_IRQHandler() {
+void USART1_IRQHandler() {
     if(USART1->STATR & USART_FLAG_RXNE) {
         // Write into buffer
         rxBufferTail = (rxBufferTail + 1) % RX_BUFFER_SIZE;
@@ -74,6 +88,6 @@ HardwareSerial::operator bool() {
             return;
         }
 
-        rxBuffer[rxBufferTail++] = USART1->DATAR & (uint16_t)0x01FF;
+        rxBuffer[rxBufferTail] = USART1->DATAR & (uint16_t)0x01FF;
     }
-}*/
+}

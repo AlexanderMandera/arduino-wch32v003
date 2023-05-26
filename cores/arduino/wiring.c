e7a9a85e699f8399d85ea3268590614b49201200
Alexander Mandera <alexander@mandera.eu>
2023-05-26 11:55:29 +0200

    Add millis() and micros()

--- cores/arduino/wiring.c
+++ cores/arduino/wiring.c
@@ -23,13 +23,28 @@
 extern "C" {
 #endif
 
+#define SYSTICK_SR_CNTIF (1<<0)
+#define SYSTICK_CTLR_STE (1<<0)
+#define SYSTICK_CTLR_STIE (1<<1)
+#define SYSTICK_CTLR_STCLK (1<<2)
+#define SYSTICK_CTLR_STRE (1<<3)
+#define SYSTICK_CTLR_SWIE (1<<31)
+
+volatile uint32_t systick_counter;
+uint32_t sysclock_div_1000 = 0;
+uint32_t sysclock_div_1000000 = 0;
+
 unsigned long millis()
 {
-    return 0;
+    return systick_counter;
 }
 
-unsigned long micros() {
-    return 0;
+unsigned long micros() 
+{
+    uint32_t currentSysTick = systick_counter;
+    uint32_t ticksDifference = (SysTick->CNT + sysclock_div_1000) - SysTick->CMP;
+
+    return currentSysTick * 1000u + ticksDifference / sysclock_div_1000000;
 }
 
 void delay(unsigned long ms)
@@ -45,6 +60,26 @@ void delayMicroseconds(unsigned int us)
 void init( void )
 {
     SystemInit48HSI();
+
+    sysclock_div_1000 = SYSTEM_CORE_CLOCK / 1000;
+    sysclock_div_1000000 = SYSTEM_CORE_CLOCK / 1000000;
+
+    SysTick->CTLR = 0;
+    NVIC_EnableIRQ(SysTicK_IRQn);
+    SysTick->CMP = (sysclock_div_1000 / 1000) - 1;
+    SysTick->CNT = 0;
+	systick_counter = 0;
+
+    SysTick->CTLR = SYSTICK_CTLR_STE | SYSTICK_CTLR_STIE |
+					SYSTICK_CTLR_STCLK;
+}
+
+void SysTick_Handler(void) __attribute__((interrupt));
+void SysTick_Handler(void)
+{
+	SysTick->CMP += (SYSTEM_CORE_CLOCK/1000);
+	SysTick->SR = 0;
+	systick_counter++;
 }
 
 #ifdef __cplusplus

98ecf952c4ef313b3e1805ed09ce413919570d08
Alexander Mandera <alexander@mandera.eu>
2023-05-31 13:49:38 +0200

    Fix copy_sources.sh for macOS

--- misc/copy_sources.sh
+++ misc/copy_sources.sh
@@ -3,4 +3,4 @@
 current_dir=$(dirname -- "$( readlink -f -- "$0"; )")
 echo $current_dir
 
-cp -ar $current_dir/../ch32v003fun/ch32v003fun $current_dir/../cores/arduino/ch32v003fun
+cp -r $current_dir/../ch32v003fun/ch32v003fun $current_dir/../cores/arduino
